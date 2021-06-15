d08691e0b5517892196c11292fa86d1a7c6812c7
gs1-karthicks <karthick.shanmugam@expeditors.com>
2021-06-15 11:56:27 +0530

    firstyear changes

--- src/app/participant-summary/firstyear/firstyear-participant.component.ts
+++ src/app/participant-summary/firstyear/firstyear-participant.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit, ViewChild, ViewEncapsulation, Input, Injector, Outpu
 import {MessageService} from 'primeng/api';
 import {Table} from "primeng/table";
 import {ActivatedRoute, Router} from "@angular/router";
+import {Participant} from "../../model/Participant";
+import {DataStorageService} from "../../services/data-storage.service";
+import {AdaptService} from "../../services/adapt.service";
+import {ParticipantStudy} from "../../model/ParticipantStudy";
 
 
 
@@ -13,35 +17,53 @@ import {ActivatedRoute, Router} from "@angular/router";
 })
 export class FirstyearParticipantComponent implements OnInit {
 
-
   @Input() activeIndex: -1;
   @Output() tabOpened: EventEmitter<any> = new EventEmitter();
   @Output() tabClosed: EventEmitter<any> = new EventEmitter();
   public baseLine: any[];
   public columnHeader: any[];
-  public tableValues : any[];
+  public tableValues: any[];
+  public participant: Participant;
   @ViewChild('dt') table: Table;
   userId = "Test";
+  public headerName;
 
-  constructor(  private route: ActivatedRoute,
-                private router: Router) { }
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private dataStorageService: DataStorageService,
+              private adaptService: AdaptService) {
+    // this.participant = this.dataStorageService.storage.participant;
+  }
 
   ngOnInit(): void {
+    this.participant = this.dataStorageService.storage.participant;
+    console.log(this.participant);
 
-    this.columnHeader=[
-      {field:'questionnaire', header :'Questionnaire'},
-      {field:'status', header :'Status'},
-      {field:'start', header :'View/Begin'},
-      {field:'completedDate', header :'Completed Date'},
-    ]
-    this.tableValues=[
-      {field:'vascularRisk', header:'Vascular Risk', value:'Not Started'},
-      {field:'cardioVascularRisk', header:'Cardio Vascular Risk', value:'Not Started'},
-      {field:'memory', header:'Memory', value:'Not Started'},
-      {field:'diet', header:'Diet', value:'Not Started'},
-      {field:'exercise', header:"Exercise", value:'Not Started' },
-    ]
+    this.columnHeader = [
+      {field: 'studyName', header: 'Questionnaire'},
+      {field: 'status', header: 'Status'},
+      {field: 'start', header: 'View/Begin'},
+      {field: 'completedDate', header: 'Completed Date'},
+    ];
+    this.loadParticipantStudyList();
+    /*    this.tableValues = [
+          {field: 'vascularRisk', header: 'Vascular Risk', value: 'Not Started'},
+          {field: 'cardioVascularRisk', header: 'Cardio Vascular Risk', value: 'Not Started'},
+          {field: 'memory', header: 'Memory', value: 'Not Started'},
+          {field: 'diet', header: 'Diet', value: 'Not Started'},
+          {field: 'exercise', header: "Exercise", value: 'Not Started'},
+        ]*/
 
+  }
+
+  loadParticipantStudyList() {
+    this.adaptService.loadParticipantStudyList(this.participant).subscribe((data: ParticipantStudy[]) => {
+      console.log(data);
+      if (data) {
+        this.headerName=this.participant.firstName;
+        this.tableValues = data;
+      }
+    });
   }
 
   onSegmentChanged(ev: any) {
@@ -64,5 +86,12 @@ export class FirstyearParticipantComponent implements OnInit {
     this.tabClosed.emit(event.index);
   }
 
+  onTab(data: any) {
+    console.log(data);
+    this.dataStorageService.storage = {
+      participantStudy: data
+    };
+
+  }
 }
 
